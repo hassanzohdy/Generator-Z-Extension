e7f98d77302b0dd7fae29cffd1b4c5e53ac05d74
Ahmed Wael <awael3977@gmail.com>
2025-02-03 22:19:47 +0200

    feat: add default export for React component templates
    - Update package.json dependencies
    - Modify React component template generation logic
    - Update yarn.lock with new dependencies

--- src/commands/generate-react-template.ts
+++ src/commands/generate-react-template.ts
@@ -1,76 +1,82 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import {
-  generateReactComponent as generateComponentApi,
-  gnz,
+	generateReactComponent as generateComponentApi,
+	gnz,
 } from "@mongez/gnz";
-import * as vscode from "vscode";
 import { ReactComponentOptions } from "@mongez/gnz/cjs/generators/generate-react-component/types";
-import { preparePath } from "../utils/prepare-path";
+import * as vscode from "vscode";
 import { getPickedOptions } from "../utils/get-picked-options";
+import { preparePath } from "../utils/prepare-path";
 
 export function generateReactComponent(
-  defaultOptions: Partial<ReactComponentOptions> = {}
+	defaultOptions: Partial<ReactComponentOptions> = {}
 ) {
-  return async function generateComponent(options: any = {}) {
-    const path = preparePath(options.path);
+	return async function generateComponent(options: any = {}) {
+		const path = preparePath(options.path);
 
-    // now ask the user to enter the name of the component name
-    const componentName = await vscode.window.showInputBox({
-      prompt: "Enter the name of the component",
-      placeHolder: "MyComponent",
-      validateInput: (text: string) => {
-        return text ? null : "Please enter a valid component name";
-      },
-    });
+		// now ask the user to enter the name of the component name
+		const componentName = await vscode.window.showInputBox({
+			prompt: "Enter the name of the component",
+			placeHolder: "MyComponent",
+			validateInput: (text: string) => {
+				return text ? null : "Please enter a valid component name";
+			},
+		});
 
-    if (!componentName) {
-      return;
-    }
+		if (!componentName) {
+			return;
+		}
 
-    const optionsList = [
-      {
-        label: "withIndex",
-        description: "Create In A Folder With Index File",
-        picked: false,
-      },
-      {
-        label: "withProps",
-        description: "With Props",
-        picked: false,
-      },
-      {
-        label: "memo",
-        description: "Wrap the component with React.memo()",
-      },
-      {
-        label: "forwardRef",
-        description: "Wrap the component with React.forwardRef()",
-        picked: false,
-      },
-    ];
+		const optionsList = [
+			{
+				label: "withIndex",
+				description: "Create In A Folder With Index File",
+				picked: false,
+			},
+			{
+				label: "withProps",
+				description: "With Props",
+				picked: false,
+			},
+			{
+				label: "memo",
+				description: "Wrap the component with React.memo()",
+			},
+			{
+				label: "exportAsDefault",
+				description: "Export The Component As Default",
+				picked: false,
+			},
+			{
+				label: "forwardRef",
+				description: "Wrap the component with React.forwardRef()",
+				picked: false,
+			},
+		];
 
-    const selectedOptions = await vscode.window.showQuickPick(optionsList, {
-      canPickMany: true,
-      placeHolder: "Select the options you want to include",
-    });
+		const selectedOptions = await vscode.window.showQuickPick(optionsList, {
+			canPickMany: true,
+			placeHolder: "Select the options you want to include",
+		});
 
-    const selectedOptionsValues = getPickedOptions(selectedOptions);
+		const selectedOptionsValues = getPickedOptions(selectedOptions);
 
-    await gnz.execute(
-      generateComponentApi.execute({
-        saveTo: path,
-        name: componentName,
-        memo: !!selectedOptionsValues.memo,
-        forwardRef: !!selectedOptionsValues.forwardRef,
-        withIndex: !!selectedOptionsValues.withIndex,
-        withProps: !!selectedOptionsValues.withProps,
-        ...defaultOptions,
-      })
-    );
+		await gnz.execute(
+			generateComponentApi.execute({
+				saveTo: path,
+				name: componentName,
+				memo: !!selectedOptionsValues.memo,
+				forwardRef: !!selectedOptionsValues.forwardRef,
+				withIndex: !!selectedOptionsValues.withIndex,
+				withProps: !!selectedOptionsValues.withProps,
+				exportAsDefault: !!selectedOptionsValues.exportAsDefault,
+				...defaultOptions,
+			})
+		);
 
-    vscode.window.showInformationMessage(
-      `Component has been generated successfully.`
-    );
-  };
+		vscode.window.showInformationMessage(
+			`Component has been generated successfully.`
+		);
+	};
 }
